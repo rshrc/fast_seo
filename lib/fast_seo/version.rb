7b11d8f0b92a60486806b67495b6865f271258a1
Rishi Banerjee <rishieric91@gmail.com>
2025-08-29 09:57:52 +0530

    initial commit
    Signed-off-by: Rishi Banerjee <rishieric91@gmail.com>

---
+++ lib/fast_seo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FastSeo
+  VERSION = "0.1.0"
+end
---
+++ spec/fast_seo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FastSeo do
+  it "has a version number" do
+    expect(FastSeo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

13038e84fe3e5aa109aa368998d0aab1f9616f52
Rishi Banerjee <rishieric91@gmail.com>
2025-08-29 10:22:22 +0530

    Refactor FastSEO module structure and enhance configuration methods

--- lib/fast_seo.rb
+++ lib/fast_seo.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
 require_relative "fast_seo/version"
+require "fast_seo/version"
+require "fast_seo/configuration"
+require "fast_seo/pages"
+require "fast_seo/schema_parts"
+require "fast_seo/providers"
+require "fast_seo/helper"
+require "fast_seo/railtie" if defined?(Rails)
 
-module FastSeo
-  class Error < StandardError; end
-  # Your code goes here...
+# The main module for the FastSEO gem.
+module FastSEO
+  class << self
+    def configure(&block)
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def page(*args, **options, &block)
+      Pages.page(*args, **options, &block)
+    end
+  end
 end
